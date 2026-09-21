a06fe3c4e3c60a666d40e30c25c71f24b298b452
ayan3432771-alt <ayan3432771@gmail.com>
2026-09-21 19:53:00 +0530

    Add files via upload

---
+++ eletriciity bill in c.cpp
@@ -0,0 +1,23 @@
+/*write a cprogramm to  calculate the eletricity bill based on the number of unit consmed
+the charges are :
+0-100=2/unit
+201-300=5/unit
+above 300=7/unit*/
+#include <stdio.h>
+int main()
+{
+    int unit;
+    float bill;
+    printf("enter the units consumed/n");
+    scanf("%d",& unit);
+    if(unit<=100)
+    bill=unit*2;
+    else if(unit<=200)
+    bill=(100*2)+(unit-100)*3;
+    else if(unit<=300)
+     bill=(100*2)+(unit-100*3)+(unit-300)*5;
+     else
+     bill=(100*2)+(unit-100*3)+(100*5)+(unit-300)*7;
+     printf("your bill amt = %.2f",bill);
+     return 0;
+ }
