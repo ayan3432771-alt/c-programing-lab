96a8b673c93eab3c0dbae036cc208079026b983c
꧁༺★𝓐𝔂𝓪𝓷★༻꧂ <ayan3432771@gmail.com>
2026-09-25 16:31:09 +0530

    Add files via upload

---
+++ while loop odd numbers 1 to n #.cpp
@@ -0,0 +1,14 @@
+//w.c.p dispiay odd numbers form 1 to n
+#include <stdio.h>
+int main()
+int n, i=1;
+printf("Enter the number:");
+scanf("%d,&n");
+printf("odd numbers form 1 to %d\n",n);
+while (i<=n)
+{
+  printf("%d\n",i);
+  i=i+2;
+  return 0
+}
+
